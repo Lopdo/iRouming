9d5ffb66dc3fb94faee4c114884d6abd94c0b886
Lope <>
2021-02-13 13:02:17 +0100

    Add actual date to comments view

--- iRouming/iRouming/Comments/CommentView.swift
+++ iRouming/iRouming/Comments/CommentView.swift
@@ -29,7 +29,7 @@ struct CommentView: View {
 
 						Spacer()
 
-						Text("20.11.2020 - 14:49")
+						Text(getStringFromDate())
 							.font(.system(size: 11, weight: .regular))
 							.foregroundColor(Color.textGray)
 					}
@@ -43,12 +43,25 @@ struct CommentView: View {
 		}.padding(16)
 		.background(Color.white)
 	}
+
+	private func getStringFromDate() -> String {
+		let df = DateFormatter()
+		if Calendar.current.isDateInToday(comment.date) {
+			df.dateFormat = "h:mm"
+		} else {
+			df.dateFormat = "dd.MM.YYYY - h:mm"
+		}
+		return df.string(from: comment.date)
+	}
 }
 
 #if DEBUG
 struct CommentView_Previews: PreviewProvider {
 	static var previews: some View {
 		CommentView(comment: Comment(nick: "RoumingUser", registered: false, date: Date(), message: "Samozřejmě první problém nastal už v tom, že ho začali ve městě honit, a on tak zbytečně ohrožoval další lidi, to máš úplnou pravdu. Tam to ani nemělo dojít.\n\nAle na tvou ironickou otázku – ano, některým lidem není příjemné, že mohou dostat odraženou střelou do hlavy jen proto, že někdo cítí potřebu střílet ve městě plném lidí po autě."))
+			.previewLayout(.sizeThatFits)
+
+		CommentView(comment: Comment(nick: "RoumingUser", registered: false, date: Date().addingTimeInterval(-60*60*24*2), message: "Samozřejmě první problém nastal už v tom, že ho začali ve městě honit, a on tak zbytečně ohrožoval další lidi, to máš úplnou pravdu. Tam to ani nemělo dojít.\n\nAle na tvou ironickou otázku – ano, některým lidem není příjemné, že mohou dostat odraženou střelou do hlavy jen proto, že někdo cítí potřebu střílet ve městě plném lidí po autě."))
 			.previewLayout(.sizeThatFits)
 	}
 }
